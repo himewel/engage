984c490cf28089707f54720bf4ecaee4b11be685
Welbert Castro <himewel@users.noreply.github.com>
2021-09-13 13:37:37 -0300

    Add connector config

---
+++ create-connector.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -i -X POST \
+    -H "Accept:application/json" \
+    -H "Content-Type:application/json" \
+    localhost:8083/connectors/ \
+    -d @./mssql-connector.json
