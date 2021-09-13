1928c43b60d923967d65703dfeaa27f81fb12053
Welbert Castro <himewel@users.noreply.github.com>
2021-09-13 17:59:19 -0300

    Create dbz-startup container

---
+++ debezium/create-connector.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "Waiting 30 seconds before startup..."
+sleep 30
+
+echo "Waiting DBZ_SOURCE to launch on ${DBZ_SOURCE}..."
+while ! nc -z ${DBZ_SOURCE//\:/ }; do
+    sleep 1
+done
+
+echo "Waiting DBZ_CONNECT to launch on ${DBZ_CONNECT}..."
+while ! nc -z ${DBZ_CONNECT//\:/ }; do
+    sleep 1
+done
+
+for file in ./connectors/*.json; do
+    curl --include \
+        --request POST \
+        --header "Accept:application/json" \
+        --header "Content-Type:application/json" \
+        --data @${file} \
+        ${DBZ_CONNECT}/connectors/
+done
