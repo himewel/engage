994f62c3c67d2bdaad41bc946c5be5d9ae6b22c5
Welbert Castro <himewel@users.noreply.github.com>
2021-09-15 21:24:17 -0300

    Add primary and foreign key tags

--- mssql/tools/create_tables.sql
+++ mssql/tools/create_tables.sql
@@ -1,130 +1,121 @@
-IF NOT EXISTS (
-    SELECT *
-    FROM sys.databases
-    WHERE name = 'engagedb'
-)
-BEGIN
-    CREATE DATABASE engagedb;
-END
-GO
+IF NOT EXISTS (SELECT *
+               FROM   sys.databases
+               WHERE  NAME = 'engagedb')
+  BEGIN
+      CREATE DATABASE engagedb;
+  END
+
+go
 
 USE engagedb;
-EXEC sys.sp_cdc_disable_db;
 
 -- GROUPS
-IF NOT EXISTS (
-    SELECT * FROM sys.tables t
-    JOIN sys.schemas s
-        ON t.schema_id = s.schema_id
-    WHERE
-        s.name = 'dbo' AND
-        t.name = 'groups'
-)
-    BEGIN
-        CREATE TABLE dbo.groups (
-            groupId        INTEGER,
-            groupName      VARCHAR(300)
+IF NOT EXISTS (SELECT *
+               FROM   sys.tables t
+                      JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+               WHERE  s.NAME = 'dbo'
+                      AND t.NAME = 'groups')
+  BEGIN
+      CREATE TABLE dbo.groups
+        (
+           groupId   INTEGER NOT NULL PRIMARY KEY,
+           groupName VARCHAR(300)
         );
-    END
+  END
 ELSE
-    BEGIN
-        TRUNCATE TABLE dbo.groups;
-    END
+  BEGIN
+      TRUNCATE TABLE dbo.groups;
+  END
 
 -- USERS
-IF NOT EXISTS (
-    SELECT * FROM sys.tables t
-    JOIN sys.schemas s
-        ON t.schema_id = s.schema_id
-    WHERE
-        s.name = 'dbo' AND
-        t.name = 'users'
-)
-    BEGIN
-        CREATE TABLE dbo.users (
-            userId        INTEGER,
-            groupId       INTEGER,
-            image         VARCHAR(300),
-            username      VARCHAR(300),
+IF NOT EXISTS (SELECT *
+               FROM   sys.tables t
+                      JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+               WHERE  s.NAME = 'dbo'
+                      AND t.NAME = 'users')
+  BEGIN
+      CREATE TABLE dbo.users
+        (
+           userId   INTEGER NOT NULL PRIMARY KEY,
+           groupId  INTEGER NOT NULL FOREIGN KEY REFERENCES groups(groupId),
+           image    VARCHAR(300),
+           username VARCHAR(300),
         );
-    END
+  END
 ELSE
-    BEGIN
-        TRUNCATE TABLE dbo.users;
-    END
+  BEGIN
+      TRUNCATE TABLE dbo.users;
+  END
 
 -- ROUNDS
-IF NOT EXISTS (
-    SELECT * FROM sys.tables t
-    JOIN sys.schemas s
-        ON t.schema_id = s.schema_id
-    WHERE
-        s.name = 'dbo' AND
-        t.name = 'rounds'
-)
-BEGIN
-        CREATE TABLE dbo.rounds (
-            roundId               INTEGER,
-            name                  VARCHAR(300),
-            roundscorebonus       INTEGER,
+IF NOT EXISTS (SELECT *
+               FROM   sys.tables t
+                      JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+               WHERE  s.NAME = 'dbo'
+                      AND t.NAME = 'rounds')
+  BEGIN
+      CREATE TABLE dbo.rounds
+        (
+           roundId         INTEGER NOT NULL PRIMARY KEY,
+           name            VARCHAR(300),
+           roundscorebonus INTEGER,
         );
-    END
+  END
 ELSE
-    BEGIN
-        TRUNCATE TABLE dbo.rounds;
-    END
+  BEGIN
+      TRUNCATE TABLE dbo.rounds;
+  END
 
 -- ACTIVITIES
-IF NOT EXISTS (
-    SELECT * FROM sys.tables t
-    JOIN sys.schemas s
-        ON t.schema_id = s.schema_id
-    WHERE
-        s.name = 'dbo' AND
-        t.name = 'activities'
-)
-    BEGIN
-        CREATE TABLE dbo.activities (
-            activityid      INTEGER,
-            roundid         INTEGER,
-            activityweight  FLOAT
+IF NOT EXISTS (SELECT *
+               FROM   sys.tables t
+                      JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+               WHERE  s.NAME = 'dbo'
+                      AND t.NAME = 'activities')
+  BEGIN
+      CREATE TABLE dbo.activities
+        (
+           activityId     INTEGER NOT NULL PRIMARY KEY,
+           roundId        INTEGER NOT NULL FOREIGN KEY REFERENCES rounds(roundId),
+           activityweight FLOAT
         );
-    END
+  END
 ELSE
-    BEGIN
-        TRUNCATE TABLE dbo.activities;
-    END
+  BEGIN
+      TRUNCATE TABLE dbo.activities;
+  END
 
 -- ANSWERS
-IF NOT EXISTS (
-    SELECT * FROM sys.tables t
-    JOIN sys.schemas s
-        ON t.schema_id = s.schema_id
-    WHERE
-        s.name = 'dbo' AND
-        t.name = 'answers'
-)
-    BEGIN
-        CREATE TABLE dbo.answers (
-            activityid          INTEGER,
-            userid              INTEGER,
-            scoreofaccuracy     FLOAT,
-            answerdatetime      DATETIME,
-            approved            VARCHAR(5)
+IF NOT EXISTS (SELECT *
+               FROM   sys.tables t
+                      JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+               WHERE  s.NAME = 'dbo'
+                      AND t.NAME = 'answers')
+  BEGIN
+      CREATE TABLE dbo.answers
+        (
+           activityId      INTEGER NOT NULL FOREIGN KEY REFERENCES activities(activityId),
+           userId          INTEGER NOT NULL FOREIGN KEY REFERENCES users(userId),
+           scoreofaccuracy FLOAT,
+           answerdatetime  DATETIME,
+           approved        VARCHAR(5)
         );
-    END
+  END
 ELSE
-    BEGIN
-        TRUNCATE TABLE dbo.users;
-    END
+  BEGIN
+      TRUNCATE TABLE dbo.users;
+  END
 
 -- enable CDC
-IF NOT EXISTS(
-    SELECT * FROM sys.databases
-    WHERE
-        is_cdc_enabled = 1 AND
-        name = 'engagedb'
-)
-BEGIN
-    EXEC sys.sp_cdc_enable_db;
-END
+IF NOT EXISTS(SELECT *
+              FROM   sys.databases
+              WHERE  is_cdc_enabled = 1
+                     AND NAME = 'engagedb')
+  BEGIN
+      EXEC sys.Sp_cdc_enable_db;
+  END
