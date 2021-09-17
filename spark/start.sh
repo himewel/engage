4b80d46161ec0da02e0141dd43d8c40186d9820a
Welbert Castro <himewel@users.noreply.github.com>
2021-09-17 16:37:56 -0300

    Add mongodb connector

--- spark/start.sh
+++ spark/start.sh
@@ -10,5 +10,6 @@ done
 spark-submit \
     --class org.apache.spark.examples.SparkPi \
     --master local \
-    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 \
+    --packages \
+        org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 \
     $SCRIPT
