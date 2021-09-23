a2dff161cab7b8eec6b87d3ba09bc7f2cc42011d
Welbert Castro <himewel@users.noreply.github.com>
2021-09-23 18:39:26 -0300

    Remove older files

--- spark/src/streaming_factory.py
+++ spark/src/streaming_factory.py
@@ -1,7 +1,7 @@
 import os
 from sys import argv
 
-from streamers import RawStreamer, JoinUsersStreamer, TrustedStreamer
+from streamers import RawStreamer, TrustedStreamer
 
 broker_server = os.getenv("BROKER_HOSTNAME")
 
@@ -9,5 +9,3 @@ if argv[1] == "raw_streamer":
     RawStreamer.create_factory(broker_server)
 elif argv[1] == "trusted_streamer":
     TrustedStreamer.create_factory()
-elif argv[1] == "join_users_streamer":
-    JoinUsersStreamer.create_factory()
