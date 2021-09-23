d9e40466b911e5882704ff4222b871755a0b71d7
Welbert Castro <himewel@users.noreply.github.com>
2021-09-23 20:25:50 -0300

    Dinammically attribute spark ui port

--- spark/start.sh
+++ spark/start.sh
@@ -9,6 +9,7 @@ done
 
 spark-submit \
     --class org.apache.spark.examples.SparkPi \
+    --conf spark.ui.port=$SPARK_UI_PORT \
     --master local \
     --packages \
         org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2,org.mongodb.spark:mongo-spark-connector_2.12:3.0.1 \
