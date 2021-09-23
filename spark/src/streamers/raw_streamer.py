e8d7b4ded22d67ff5c415d69794001d43f073640
Welbert Castro <himewel@users.noreply.github.com>
2021-09-23 18:31:44 -0300

    Pass parameters by constructor method

--- spark/src/streamers/raw_streamer.py
+++ spark/src/streamers/raw_streamer.py
@@ -4,13 +4,19 @@ from . import AbstractStreamer
 
 
 class RawStreamer(AbstractStreamer):
-    def get_process(self, broker_server, topic_name):
+    def __init__(self, broker_server, topic_name):
+        super().__init__()
+        self.broker_server = broker_server
+        self.topic_name = topic_name
+        self.schema = self.schema_classes[topic_name]
+
+    def get_process(self):
         spark = self.get_spark()
 
         df = (
             spark.readStream.format("kafka")
-            .option("kafka.bootstrap.servers", broker_server)
-            .option("subscribe", topic_name)
+            .option("kafka.bootstrap.servers", self.broker_server)
+            .option("subscribe", self.topic_name)
             .option("startingOffsets", "earliest")
             .load()
         )
@@ -18,19 +24,19 @@ class RawStreamer(AbstractStreamer):
         for column in ["key", "value"]:
             df = df.withColumn(column, col(column).cast("string"))
 
-        df = (
-            self.schema_classes[topic_name]
-            .explode(dataframe=df, src_column="value", dst_column="payload")
-            .select("payload.after.*", "timestamp")
-        )
+        df = self.schema.explode(
+            dataframe=df, src_column="value", dst_column="payload"
+        ).select("payload.after.*", "timestamp")
 
         long_columns = [field[0] for field in df.dtypes if field[1] == "bigint"]
         for column in long_columns:
-            df = df.withColumn(column, from_unixtime(col(column) / 1000))
+            df = df.withColumn(
+                column, from_unixtime(col(column) / 1000).cast("timestamp")
+            )
 
-        process = df.writeStream.queryName(f"subscribe_{topic_name}").start(
-            path=f"/raw/{topic_name}",
-            checkpointLocation=f"/checkpoint/raw/{topic_name}",
+        process = df.writeStream.queryName(f"raw_{self.topic_name}").start(
+            path=f"/raw/{self.topic_name}",
+            checkpointLocation=f"/checkpoint/raw/{self.topic_name}",
             mode="append",
         )
 
@@ -40,8 +46,8 @@ class RawStreamer(AbstractStreamer):
         table_list = ["groups", "users", "activities", "answers", "rounds"]
         topic_list = [f"engagedb.dbo.{table}" for table in table_list]
 
-        streamer = RawStreamer()
         for topic_name in topic_list:
-            streamer.get_process(broker_server, topic_name)
+            streamer = RawStreamer(broker_server, topic_name)
+            streamer.get_process()
 
         streamer.stream()
