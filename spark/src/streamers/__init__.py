b6b9ce5c8c7e9372620302e5fc21c5a3ea35b176
Welbert Castro <himewel@users.noreply.github.com>
2021-09-22 22:49:55 -0300

    Add join users streaming job

--- spark/src/streamers/__init__.py
+++ spark/src/streamers/__init__.py
@@ -1,2 +1,3 @@
 from .abstract_streamer import AbstractStreamer
 from .raw_streamer import RawStreamer
+from .join_users_streamer import JoinUsersStreamer
--- spark/src/streaming_factory.py
+++ spark/src/streaming_factory.py
@@ -1,9 +1,11 @@
 import os
 from sys import argv
 
-from streamers import RawStreamer
+from streamers import RawStreamer, JoinUsersStreamer
 
 broker_server = os.getenv("BROKER_HOSTNAME")
 
 if argv[1] == "raw_streamer":
-    streamer = RawStreamer.create_factory(broker_server)
+    RawStreamer.create_factory(broker_server)
+elif argv[1] == "join_users_streamer":
+    JoinUsersStreamer.create_factory()
