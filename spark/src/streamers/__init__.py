0f9b5a04636dd6c2eef24eb3f0147b187a63afb9
Welbert Castro <himewel@users.noreply.github.com>
2021-09-24 17:10:06 -0300

    Add aggregator streamer

--- spark/src/streamers/__init__.py
+++ spark/src/streamers/__init__.py
@@ -1,3 +1,4 @@
 from .abstract_streamer import AbstractStreamer
 from .raw_streamer import RawStreamer
 from .trusted_streamer import TrustedStreamer
+from .groups_streamer import GroupsStreamer
--- spark/src/streaming_factory.py
+++ spark/src/streaming_factory.py
@@ -1,7 +1,7 @@
 import os
 from sys import argv
 
-from streamers import RawStreamer, TrustedStreamer
+from streamers import RawStreamer, TrustedStreamer, GroupsStreamer
 
 broker_server = os.getenv("BROKER_HOSTNAME")
 
@@ -9,3 +9,5 @@ if argv[1] == "raw_streamer":
     RawStreamer.create_factory(broker_server)
 elif argv[1] == "trusted_streamer":
     TrustedStreamer.create_factory()
+elif argv[1] == "groups_streamer":
+    GroupsStreamer.create_factory()
