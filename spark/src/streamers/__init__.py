d63cc92127b988253f713874f9cac326b6df67e4
Welbert Castro <himewel@users.noreply.github.com>
2021-10-04 21:34:29 -0300

    Remove deprecated files

--- spark/src/streamers/__init__.py
+++ spark/src/streamers/__init__.py
@@ -1,4 +1,2 @@
 from .abstract_streamer import AbstractStreamer
 from .raw_streamer import RawStreamer
-from .trusted_streamer import TrustedStreamer
-from .groups_streamer import GroupsStreamer
--- spark/src/streaming_factory.py
+++ spark/src/streaming_factory.py
@@ -1,13 +1,9 @@
 import os
 from sys import argv
 
-from streamers import RawStreamer, TrustedStreamer, GroupsStreamer
+from streamers import RawStreamer
 
 broker_server = os.getenv("BROKER_HOSTNAME")
 
 if argv[1] == "raw_streamer":
     RawStreamer.create_factory(broker_server)
-elif argv[1] == "trusted_streamer":
-    TrustedStreamer.create_factory()
-elif argv[1] == "groups_streamer":
-    GroupsStreamer.create_factory()
