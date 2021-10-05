8d8e05366ab83ab1a563fe4fcecff7dd0e8564fd
Welbert Castro <himewel@users.noreply.github.com>
2021-10-05 17:28:56 -0300

    Create python start script

---
+++ spark/scripts/python-start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Waiting kafka broker to launch on ${BROKER_HOSTNAME}..."
+while ! nc -z ${BROKER_HOSTNAME//\:/ }; do
+    sleep 1
+done
+
+echo "Waiting hadoop to launch on ${HADOOP_HOSTNAME}..."
+while ! nc -z ${HADOOP_HOSTNAME//\:/ }; do
+    sleep 1
+done
+
+python3 $SPARK_HOME/$@
