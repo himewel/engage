fd81ec3c287fda80dc58173a33ef328203ccbac5
Welbert Castro <himewel@users.noreply.github.com>
2021-10-05 17:29:35 -0300

    Convert mongo queries to json files

--- spark/src/queries/group_scores.js
+++
@@ -1,9 +0,0 @@
-db.userScores.aggregate([
-    {
-        $group: {
-            _id: { groupId: "$_id.groupId" },
-            groupScore: { $sum: "$roundScores.roundScore" }
-        }
-    },
-    { $merge: { into: "groupScores", on: ["_id"] } }
-]);
