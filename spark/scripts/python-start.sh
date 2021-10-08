351174fffeb30d2a6c5371ddf8db7a1bceb46066
Welbert Castro <himewel@users.noreply.github.com>
2021-10-08 15:14:18 -0300

    Remove volumes from streaming containers

--- spark/scripts/python-start.sh
+++ spark/scripts/python-start.sh
@@ -7,9 +7,4 @@ while ! nc -z ${BROKER_HOSTNAME//\:/ }; do
     sleep 1
 done
 
-echo "Waiting hadoop to launch on ${HADOOP_HOSTNAME}..."
-while ! nc -z ${HADOOP_HOSTNAME//\:/ }; do
-    sleep 1
-done
-
 python3 $SPARK_HOME/$@
